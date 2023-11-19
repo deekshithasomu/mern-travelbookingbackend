92194123222734f50178d5cd7eb5c7331d6a05ed
deekshithasomu <144221159+deekshithasomu@users.noreply.github.com>
2023-11-19 12:57:29 +0530

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -20,7 +20,7 @@ export const register = async (req, res) => {
 
       res.status(200).json({ success: true, message: "Successfully created!" })
    } catch (error) {
-      res.status(500).json({ success: false, message: "Failed to create! Try again." })
+      res.status(500).json({ success: false, message: "Successfully created" })
    }
 }
 
