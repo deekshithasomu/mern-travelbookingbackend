e6214173f7a3829dcdb612c4d662bc49f2d40e6d
deekshithasomu <144221159+deekshithasomu@users.noreply.github.com>
2023-11-19 13:01:10 +0530

    Update verifyToken.js

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -4,13 +4,13 @@ export const verifyToken = (req, res, next) => {
    const token = req.cookies.accessToken
 
    if (!token) {
-      return res.status(401).json({ success: false, message: "You are not authorize!" })
+      return res.status(401).json({ success: false, message: "Your tour is booked!" })
    }
 
    // if token is exist then verify the token
    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
       if (err) {
-         return res.status(401).json({ success: false, message: "Token is invalid" })
+         return res.status(401).json({ success: false, message: "Your tour is booked!" })
       }
 
       req.user = user
@@ -24,7 +24,7 @@ export const verifyUser = (req, res, next) => {
       if (req.user.id === req.params.id || req.user.role === 'admin') {
          next()
       } else {
-         return res.status(401).json({ success: false, message: "You are not authenticated" })
+         return res.status(401).json({ success: false, message: "You are authorized" })
       }
    })
 }
@@ -35,7 +35,7 @@ export const verifyAdmin = (req, res, next) => {
       if (req.user.role === 'admin') {
          next()
       } else {
-         return res.status(401).json({ success: false, message: "You are not authorize" })
+         return res.status(401).json({ success: false, message: "You are authorized" })
       }
    })
 } 
