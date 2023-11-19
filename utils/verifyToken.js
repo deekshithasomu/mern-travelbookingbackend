ae53770a179993f731a0309ddca9ae312d8907ab
deekshithasomu <144221159+deekshithasomu@users.noreply.github.com>
2023-11-19 13:17:18 +0530

    Update verifyToken.js

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -24,7 +24,7 @@ export const verifyUser = (req, res, next) => {
       if (req.user.id === req.params.id || req.user.role === 'admin') {
          next()
       } else {
-         return res.status(401).json({ success: false, message: "You are authorized" })
+         return res.status(401).json({ success: false, message: "Your tour is booked" })
       }
    })
 }
@@ -35,7 +35,7 @@ export const verifyAdmin = (req, res, next) => {
       if (req.user.role === 'admin') {
          next()
       } else {
-         return res.status(401).json({ success: false, message: "You are authorized" })
+         return res.status(401).json({ success: false, message: "Your tour is booked" })
       }
    })
 } 
